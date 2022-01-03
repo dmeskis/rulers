8602263c9465334cf9d948035e27d755b69c26fc
Dylan <dmeskis@gmail.com>
2022-01-03 16:08:18 -0700

    chapter 1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require 'rulers/array'
 
 module Rulers
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Application
+    def call(env)
+      `echo debug > debug.txt`
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+    end
+  end
 end
